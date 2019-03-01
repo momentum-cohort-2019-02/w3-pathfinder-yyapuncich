2db905f61bcabfe11eb93aec6eeee431a51875e2
Yifaa Yapuncich <yifaasy@gmail.com>
2019-02-28 22:01:47 -0500

    just planning out my classes

---
+++ pathfinder.py
@@ -0,0 +1,17 @@
+import pytest
+import pillow
+
+class Map:
+    """
+    Reads text file of map. Generates output of elevation by color variation in image file.
+    """
+
+class MapImage:
+    """
+    Creates the lists and method used to generate the map.
+    """
+
+class Pathfinder:
+    """
+    Draws a path from the westernmost axis on the map to a destination on the easternmost axis. Looks for next step by searching for elevation value closest to current elevation value, with 3 coordinates, always moving east.
+    """
