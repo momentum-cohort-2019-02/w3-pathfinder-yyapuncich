ddb2649e3db9b3f1fb03e5678f1d8a0684eca642
Yifaa Yapuncich <yifaasy@gmail.com>
2019-03-01 14:46:08 -0500

    got some methods defined for Coordinates

--- pathfinder.py
+++ pathfinder.py
@@ -1,17 +1,57 @@
-import pytest
-import pillow
+import PIL
 
-class Map:
+class Coordinates:
     """
-    Reads text file of map. Generates output of elevation by color variation in image file.
+    Creates the lists and methods used to generate the map.
     """
+    def __init__(self, filename):
+        """Sets methods to variables in Coordinates class"""
+        self.elevations = self.get_elevations(filename)
+        self.max_elevation = self.get_max_elevation()
+        self.min_elevation = self.get_min_elevation()
+        self.color_value = self.get_color_value()
+        
+    def get_elevations(self, filename):
+        """Gets total list of elevations from file"""
+        with open(filename) as file:
+            self.elevations = [[int(e) for e in line.split(" ")] for line in file]
+        return self.elevations
+    
+    def get_max_elevation(self):
+        """Gets maximun elevation in from file in get_elevations"""
+        self.max_elevation = max([max(row) for row in self.elevations])
+        return self.max_elevation
+
+    def get_min_elevation(self):
+        """Gets minimum elevation in from file in get_elevations"""
+        self.min_elevation = min([min(row) for row in self.elevations])
+        return self.min_elevation
+
+    def get_color_value(self):
+        """Assigns color value to coordinate based on elevation"""
+        # Not sure I'm putting the color_number in the right spot right now... we'll see what colors come out.
+        current_elevation = 3500
+        color_number = int((current_elevation - self.min_elevation) / (self.max_elevation - self.min_elevation) * 255)
+        self.color_value = (color_number, color_number, color_number, 0)
+        return self.color_value
+
+    # def draw_map(self, color_values):
+
 
 class MapImage:
     """
-    Creates the lists and method used to generate the map.
+    Reads text file of map. Generates output of elevation by color variation in image file.
     """
+    
 
 class Pathfinder:
     """
     Draws a path from the westernmost axis on the map to a destination on the easternmost axis. Looks for next step by searching for elevation value closest to current elevation value, with 3 coordinates, always moving east.
     """
+
+if __name__ == "__main__":
+
+    my_map = Coordinates('elevation_small.txt')
+
+    print(my_map.max_elevation, my_map.min_elevation, my_map.color_value)
+
---
+++ test_pathfinder.py
@@ -0,0 +1,8 @@
+import pytest
+import random
+from pathfinder import MapInfo, MapImage
+
+
+# def test_elevation_list(): I DON'T KNOW IF I CAN TEST THIS
+#     elevations_list = MapInfo.elevation('elevation_small.txt')
+#     assert elevations_list == list([])
