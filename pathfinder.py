ab5a2071f8e16d8be0d578b5dbccd1da66cb6323
Yifaa Yapuncich <yifaasy@gmail.com>
2019-03-02 21:10:35 -0500

    well i have a white square and that's something

--- pathfinder.py
+++ pathfinder.py
@@ -1,6 +1,4 @@
 from PIL import ImageColor, ImageDraw, Image
-import random
-import pprint
 
 class MapInfo:
     """
@@ -11,9 +9,12 @@ class MapInfo:
         self.elevations = self.get_elevations(filename)
         self.max_elevation = self.get_max_elevation()
         self.min_elevation = self.get_min_elevation()
+        # self.current_elevation = self.get_current_elevation()
+        # self.color_value = self.get_color_value()
         
     def get_elevations(self, filename):
         """Gets total list of elevations from file"""
+        # Creating my 2D list- understanding it: https://snakify.org/de/lessons/two_dimensional_lists_arrays/
         with open(filename) as file:
             self.elevations = [[int(e) for e in line.split(" ")] for line in file]
         return self.elevations
@@ -28,17 +29,6 @@ class MapInfo:
         self.min_elevation = min([min(row) for row in self.elevations])
         return self.min_elevation
 
-    def get_current_elevation(self, x, y):
-        """Gets elevation at one point to create color pixel for that coordinate"""
-        # figure out how this works ?????
-        return self.elevations[y][x]
-
-    def get_color_value(self, x, y):
-        """Assigns color value to coordinate based on current elevation"""
-        # Not sure I'm putting the color_number in the right spot right now... we'll see what colors come out. Maybe move this to MapImage class
-        color_number = int((self.get_current_elevation(x, y) - self.min_elevation) / (self.max_elevation - self.min_elevation) * 255)
-        self.color_value = (color_number, color_number, color_number, 0)
-        return self.color_value
 
 class MapImage:
     """
@@ -49,14 +39,23 @@ class MapImage:
         # had to add len() to list info here so it would return a number for creating size on new image.
         self.im = Image.new('RGBA', (len(self.my_map.elevations[0]), len(self.my_map.elevations)))
         # self.new_image = draw_map()
+    def get_current_elevation(self, x, y):
+        return self.my_map.elevations[y][x]
+
+    def get_color_value(self, x, y):
+        """Assigns color value to coordinate based on current elevation"""
+        # Not sure I'm putting the color_number in the right spot right now... we'll see what colors come out. Maybe move this to MapImage class
+        # current_elevation = 0
+        return int((self.get_current_elevation(x, y)) - (self.my_map.min_elevation) / (self.my_map.max_elevation - self.my_map.min_elevation) * 255)
 
     def draw_map(self):
         """For elevations listed in elevation list use the color value to draw the map."""
         # What is Numpy? Saw it in my searches about putpixel...
-        for y in range(len(self.my_map.elevations)):
-            self.im.putpixel((x,y), (self.my_map.color_value(x,y)))
-            self.im.save('my_map.png')
-        
+        # automate boring stuff- putpixel step 3.
+        for x in range(len(self.my_map.elevations[0])):
+            for y in range(len(self.my_map.elevations)):
+                self.im.putpixel((x, y), ((self.get_color_value(x, y)), (self.get_color_value(x, y)), (self.get_color_value(x, y))))
+        return self.im.save('new_map.png')
 
 
 class Pathfinder:
@@ -67,6 +66,7 @@ class Pathfinder:
 if __name__ == "__main__":
 
 # instantiate (def: represent as or by an instance)
+
     my_map = MapInfo('elevation_small.txt')
     drawing = MapImage(my_map)
     drawing.draw_map()
