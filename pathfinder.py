0f04d3a409f659ebde74f285c87d041219fb2b47
Yifaa Yapuncich <yifaasy@gmail.com>
2019-03-03 21:05:07 -0500

    wrote a few tests

--- pathfinder.py
+++ pathfinder.py
@@ -9,8 +9,6 @@ class MapInfo:
         self.elevations = self.get_elevations(filename)
         self.max_elevation = self.get_max_elevation()
         self.min_elevation = self.get_min_elevation()
-        # self.current_elevation = self.get_current_elevation()
-        # self.color_value = self.get_color_value()
         
     def get_elevations(self, filename):
         """Gets total list of elevations from file"""
@@ -67,9 +65,10 @@ class Pathfinder:
 
     def get_route(self):
         """Creates list of potential next steps based on current location. Also creates diff_list that will be used to find min difference for next step"""
+        # couldn't get this to work when I broke up this method into smaller parts, but it works right now
         cur_x = 0
-        cur_y = 150
-        # for cur_y in len(self.my_map.elevations[0]):
+        cur_y = 350
+        # not sure why -1 is needed below?
         while cur_x < len(self.my_map.elevations[0]) - 1:
             potential_ys = [cur_y]
             if cur_y - 1 >= 0:
@@ -78,26 +77,22 @@ class Pathfinder:
                 potential_ys.append(cur_y + 1)
         # diff_list get's list of difference in elevation values from (next column potenial steps) - (current column location/elevation)  
             self.diff_list = [abs(self.my_map.elevations[pot_y][cur_x + 1] - self.my_map.elevations[cur_y][cur_x]) for pot_y in potential_ys]
-
+        # bear with me: the min_diff gets the value from next potential_y that is best, the min_diff_index saves that location on the diff_list, the next_y selects the potenial_y that matches that same location of the min_diff_index...
             min_diff = min(self.diff_list)
             min_diff_index = self.diff_list.index(min_diff)
             next_y = potential_ys[min_diff_index]
             #create my list of coordinates for the path 
             line_creator = ImageDraw.Draw(self.draw_line)
             line_creator.point((cur_x, cur_y), (240, 255, 140))
-
+        # getting all the points that we'll need to draw the dang route
             cur_x += 1
             cur_y = next_y
         return self.draw_line.save('drawn_line.png')
-            # print("cur_x", cur_x)
-            # print("cur_y", cur_y)
+
 if __name__ == "__main__":
-
 # instantiate (def: represent as or by an instance)
-
     my_map = MapInfo('elevation_small.txt')
-    drawing = MapImage(my_map)
-    drawing.draw_map()
+    my_map_image = MapImage(my_map)
+    my_map_image.draw_map()
     path_it_out = Pathfinder(my_map, 'new_map.png')
     path_it_out.get_route()
-    print(my_map.max_elevation, my_map.min_elevation)
--- test_pathfinder.py
+++ test_pathfinder.py
@@ -1,8 +1,17 @@
 import pytest
 import random
-from pathfinder import MapInfo, MapImage
+from pathfinder import MapInfo, Pathfinder, MapImage
 
 
-# def test_elevation_list(): I DON'T KNOW IF I CAN TEST THIS
-#     elevations_list = MapInfo.elevation('elevation_small.txt')
-#     assert elevations_list == list([])
+my_map = MapInfo('elevation_small.txt')
+my_map_image = MapImage(my_map)
+path_it_out = Pathfinder(my_map, 'new_map.png')
+
+def test_get_max_elevation():
+    assert my_map.get_max_elevation() == 5648
+
+def test_get_min_elevation():
+    assert my_map.get_min_elevation() == 3139
+
+def test_color_value():
+    assert my_map_image.get_color_value(3, 6) in range(1, 255)
